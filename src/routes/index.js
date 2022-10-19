4de678eec456272a711ab16007f7f544240cf74c
Santiago <98236588+Santy1707@users.noreply.github.com>
2022-10-18 20:25:47 -0500

    Cambio de 500 a 400 en la ruta post

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,15 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
 const axios = require ('axios');
-//const Pokemon = require('../models/Pokemon');
 const router = Router();
 const {Pokemon, Types} = require('../db');
 const {getAllPokemons} = require ('../funciones.js/functions')
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 
 // ---------------------
 
 router.get('/pokemons', async (req, res) => {
     const { name } = req.query 
     try {
-       // const id = req.params.id
         const pokemonsTotal = await getAllPokemons()
         if (name) {
             const pokemonName = pokemonsTotal.filter(
@@ -25,14 +19,10 @@ router.get('/pokemons', async (req, res) => {
            res.status(200).send(['No se encuentra el personaje con dicho nombre'])
         }
         else {
-         //   let baseDeDatos = await getDbInfo()
-         //   let hola = baseDeDatos;
-         //   console.log(hola)
             res.status(200).send(pokemonsTotal)
         }
     } catch (error) {
         res.status(500).send(error.message)
-   // 
     }
 })
 
@@ -42,8 +32,6 @@ router.get('/pokemons', async (req, res) => {
 router.get('/pokemons/:id', async (req, res, next) => {
     const {id} = req.params
     try {
-        // esta funcion tiene tanto API COMO BASE DE DATOS, toda la informacion, ya la probé y funciona
-        // console.log(pokemonsTotal)
     getAllPokemons()
     .then(data => { 
         let pokeId = data.filter((e) => {
@@ -54,12 +42,6 @@ router.get('/pokemons/:id', async (req, res, next) => {
     res.status(200).json(pokeId) : 
     res.status(404).send('No se encuentra el pokemon con dicho id')
     })
-    // console.log(pokeId)
-            // console.log(pokeId + 'hola no encontro el ID')
-        //else if (pokeIdApi.length){
-             //   res.status(200).json(pokeIdApi) 
-            // } 
-          //   console.log(pokeIdApi)
         }
      catch (error) {
         next('no se encontro el pokemon con ese ID', error)
@@ -90,21 +72,15 @@ router.post('/pokemons', async (req, res) => {
         const { name, hp, attack, defense, speed,
             height, weight, created, types, image
         } = req.body
-        // console.log(types)
         const pokemon = await Pokemon.create({name, hp, attack, defense, speed, height, weight, created, image 
         })
-       // console.log(pokemon)
         const typesDb = await Types.findAll({
             where: {name: types}
         })
-      // typesArr = typesDb //Object.keys(typesDb) 
-       // console.log(typesArr)*/
-        //console.log(typesArr)
         pokemon.addTypes(typesDb)
-      //  console.log(pokemon)
         res.status(200).send('Personaje creado con exito, sos un crack!!')
     } catch (error) {
-        res.status(500).send(error)
+        res.status(400).send(error)
     }
 })
 
@@ -143,10 +119,6 @@ router.put('/pokemons/:id', async (req, res) => {
         console.log(error)
     }
 })
-// const {id} = req.params;
-// const pk = Pokemon.findByPk(id)
-// if (pk)
-// pk.destroy()
-// res.status(200).send('Pokemon eliminado')
+
 
 module.exports = router;
